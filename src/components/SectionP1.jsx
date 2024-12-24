b3219a08e59655439ac99d2389bb82eea44bfd99
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-24 12:36:14 -0400

    Componente que contiene la parte 1 de la section dos.

---
+++ src/components/SectionP1.jsx
@@ -0,0 +1,17 @@
+
+
+function SectionP1(props){
+
+    return(
+        
+        
+        <section>
+                <div className="card">
+                    <img src={props.ImagenCard} alt="Imgen-card" />
+                    <h3>{props.tituloCard}</h3>
+                    <p>{props.ContenidoCard}</p>
+                </div>
+            </section>
+    )
+}
+export default SectionP1

b5fd25efe9bffd91847dc6162e887775bff5c767
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-24 12:33:28 -0400

    componente header.

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+
+function Header(){
+    return(
+        <section>
+                    <div>
+                        <h1>
+                            Logo1
+                        </h1>
+                    </div>
+                    <div>
+                        <nav>
+                            <ul>
+                                <li>Opcion 1</li>
+                                <li>Opcion 2</li>
+                                <li>Opcion 3</li>
+                                <li>Opcion 4</li>
+                            </ul>
+                        </nav>
+                    </div>
+        </section>
+    )
+}
+export default Header

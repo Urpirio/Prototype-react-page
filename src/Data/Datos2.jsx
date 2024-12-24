47789e7aee0da2d41c46a95bd748b462faa337bf
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-24 12:40:11 -0400

    Componente Array que actua como base de  datos que  manda contenido a la section numero dos.

---
+++ src/Data/Datos2.jsx
@@ -0,0 +1,19 @@
+
+const SectionP1_Datos = [
+    {
+        Imagen: "src/assets/cripto.png",
+        titulo: "Titulo 1",
+        Contenido: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ullamcorper orci, quis porttitor mauris."
+    },
+    {
+        Imagen:"src/assets/camara.png",
+        titulo: "Titulo 2",
+        Contenido: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ullamcorper orci, quis porttitor mauris."
+    },
+    {
+        Imagen: "src/assets/money-management.png",
+        titulo: "Titulo 3",
+        Contenido: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ullamcorper orci, quis porttitor mauris."
+    }
+]
+export default SectionP1_Datos

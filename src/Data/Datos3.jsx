1cbabdf66403607f67525534426323909a49661b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-24 12:40:47 -0400

    Componente Array que manda contenido a la Section 2, Parte 2

---
+++ src/Data/Datos3.jsx
@@ -0,0 +1,19 @@
+const DivData = [
+    {
+        Numeros: "600",
+        Name: "Estadistica 1"
+    },
+    {
+        Numeros:"585",
+        Name: "Estadistica 2"
+    },
+    {
+        Numeros: "560",
+        Name: "Estadistica 3"
+    },
+    {
+        Numeros: "280",
+        Name: "Estadistica 4"
+    }
+]
+export default DivData

7d5623d85a0dd999b2bfe627c06915f5e3ab049a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-24 12:32:49 -0400

    Componente Principal  de la pagina.
    Aqui llegan todos los sub componentes antes de renderizar la pagina.

---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import "../style/Home.css";
+import Header from "./Header";
+import Section from "./Main.jsx";
+import datos from "../Data/Datos1.jsx"
+
+function Home(){
+    const datos_main_section1 = datos.map( Datosmain1 => {
+        return <Section titulo ={Datosmain1.titulo} Contenido={Datosmain1.contenido} />
+    })
+    return(
+        <section>
+            <header>
+                <Header/>
+            </header>
+            <main>
+                {datos_main_section1}
+
+            </main>
+            <footer>
+                
+            </footer>
+        </section>
+    )
+}
+export default Home

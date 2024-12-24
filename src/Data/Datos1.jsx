59ec83e9d63793aea3c98671dd785a2fa09b8122
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-24 12:38:54 -0400

    Componente Array, que actua como base de datos y manda contenido a la Section 1.

---
+++ src/Data/Datos1.jsx
@@ -0,0 +1,8 @@
+const datos = [
+    {
+        titulo: "Marking Your Business Ideas Come True",
+        contenido: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ullamcorper orci, quis porttitor mauris. Vestibulum congue et massa et maximus. "
+    },
+  
+]
+export default datos

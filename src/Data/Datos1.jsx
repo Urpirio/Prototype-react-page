e96304ee6e30cbd3ad71ddb231167de7ad6b58f7
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-25 20:00:13 -0400

    no hubo cambios.

--- src/Data/Datos1.jsx
+++ src/Data/Datos1.jsx
@@ -3,6 +3,5 @@ const datos = [
         titulo: "Marking Your Business Ideas Come True",
         contenido: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ullamcorper orci, quis porttitor mauris. Vestibulum congue et massa et maximus. "
     },
-  
 ]
 export default datos
